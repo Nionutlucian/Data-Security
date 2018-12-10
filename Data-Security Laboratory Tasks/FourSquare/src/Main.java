6855e6cdfc12a96d53311c01bc2b897486916b1e
adriana 96 <ciocioiadriana@yahoo.com>
2018-12-10 13:35:41 +0200

    FourSquareCipher`

---
+++ Data-Security Laboratory Tasks/FourSquare/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String args[]) {
+		FourSquareCipher fsc = new FourSquareCipher();
+		System.out.println(fsc.encryptPlaintTextInput("ANA ARE MERE"));
+		System.out.println(fsc.decryptCipherTextInput("ANA ARE MERE"));
+	}
+}
