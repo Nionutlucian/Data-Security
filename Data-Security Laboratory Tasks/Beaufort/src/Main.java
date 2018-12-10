dd64ec6cd18bedb0e3155df51063f1c4741364f8
adriana 96 <ciocioiadriana@yahoo.com>
2018-12-10 13:52:20 +0200

    BeaufortCipher

---
+++ Data-Security Laboratory Tasks/Beaufort/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		BeaufortCipher bfc = new BeaufortCipher();
+		//System.out.println(bfc.encrypt("ANA", "ANA ARE MERE"));
+		System.out.println(bfc.decrypt("ANA", "AAAAWWOJJW"));
+
+	}
+
+}
