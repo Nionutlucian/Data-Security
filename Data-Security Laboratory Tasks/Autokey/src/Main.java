4c8b7ddeb80359b9ba89c05f08d5bcdd50654f4f
adriana 96 <ciocioiadriana@yahoo.com>
2018-12-10 13:42:50 +0200

    AutoKeyCipher

---
+++ Data-Security Laboratory Tasks/Autokey/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		AutoKeyCipher akc = new AutoKeyCipher();
+		//System.out.println(akc.encryptPlaintTextInput("ANA ARE MERE"));
+		System.out.println(akc.decryptCipherTextInput("ANA ARE MERE"));
+
+	}
+
+}
